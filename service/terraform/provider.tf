44924ebf501ed25ca92284697081d85ef03f9d38
goncalofonseca <goncalofnsc@gmail.com>
2022-02-07 22:51:49 +0000

    circleci

--- service/terraform/provider.tf
+++ service/terraform/provider.tf
@@ -3,7 +3,7 @@
 # ---------------------------------------------------------------------------------------------------------------------
 
 terraform {
-  required_version = "1.1.5"
+  required_version = "1.1.3"
   backend "s3" {
     region = "eu-west-3"
     key = "ecs-service/terraform.tfstate"
